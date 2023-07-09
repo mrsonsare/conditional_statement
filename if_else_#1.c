2584cf2bf71dcb999249abe377079667eea087d6
Mr.Sonsare <135420501+mrsonsare@users.noreply.github.com>
2023-07-09 23:17:56 +0530

    Create if_else_#1.c

---
+++ if_else_#1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+  int n ;
+  printf("Enter a Number:");
+  scanf("%d",&n);
+  if (n > 10)
+    printf("Hello Guys");
+  else 
+    printf("Hello World");
+    printf("\nMark Sheet");
+  return 0;
+}
