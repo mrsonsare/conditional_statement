d1c58c0e6091c540a9c96569eb15bb13f1ff3da2
Mr.Sonsare <135420501+mrsonsare@users.noreply.github.com>
2023-07-09 23:20:04 +0530

    Create if_startup.c

---
+++ if_startup.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+  int a ;
+  printf("Enter a number :");
+  scanf("%d",&a);
+  if (a > 10) {
+    printf("Mission Successful");
+  }
+  printf("Hello World\n");
+  return 0;
+}
